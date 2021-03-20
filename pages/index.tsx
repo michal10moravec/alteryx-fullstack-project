c861f4e443a1589e3a7b3aff7b20b8c2d737d49c
Michal Moravec <michal10moravec@gmail.com>
2021-03-20 11:12:52 +0100

    Added material-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import Box from '@material-ui/core/Box'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import React from 'react'
+
 const Index = () => {
-  return <></>
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js with TypeScript example
+        </Typography>
+      </Box>
+    </Container>
+  )
 }
 
 export default Index
